571d8ec85ad5b78efc865f573a666dc3eac6fd01
Richard <droste.richard@gmail.com>
2018-01-17 22:10:52 +0000

    Save result in cache.txt to speed up future lookups

--- ncitations.sh
+++ ncitations.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-title="$(xclip -out -selection primary)"
 DIR="$(dirname $0)"
-ncite="$(python $DIR/scholar.py -c 1 -t --phrase "$title" | awk '/Citations [0-9]/ { print $2 }')"
+CACHEFILE="$DIR/chache.txt"
 
-if [ -z $ncite ]; then
+TITLE="$(xclip -out -selection primary)"
+
+HASH="$(echo $TITLE | md5sum | awk '{print $1}')"
+NCITE="$(awk -vLOOKUPVAL=$HASH '$1 == LOOKUPVAL { print $2 }' < $CACHEFILE)"
+
+if [ -z $NCITE ]; then
+	NCITE="$(python $DIR/scholar.py -c 1 -t --phrase "$TITLE" | awk '/Citations [0-9]/ { print $2 }')"
+	[ -z $NCITE ] || echo "$HASH $NCITE" >> $CACHEFILE
+fi
+
+if [ -z $NCITE ]; then
 	xmessage -buttons "Ok:0" -default Ok -nearmouse "Paper not found" -timeout 5
 	exit 0
 fi
 
-message="No. citations: $ncite"
-xmessage -buttons "Ok:0,Open in browser:1" -default Ok -nearmouse "$message" -timeout 10
-response=$?
+MESSAGE="No. citations: $NCITE"
+xmessage -buttons "Ok:0,Open in browser:1" -default Ok -nearmouse "$MESSAGE" -timeout 10
+RESPONSE=$?
 
-if [ "$response" = "1" ]; then
-	sensible-browser "https://scholar.google.ch/scholar?&q=${title// /+}"
+if [ "$RESPONSE" = "1" ]; then
+	sensible-browser "https://scholar.google.ch/scholar?&q=${TITLE// /+}"
 fi
