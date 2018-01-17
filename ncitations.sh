81611571d59f523d2754069456b9210f70e50094
Richard <droste.richard@gmail.com>
2018-01-17 21:06:36 +0000

    Indicate if paper not found

--- ncitations.sh
+++ ncitations.sh
@@ -4,6 +4,11 @@ title="$(xclip -out -selection primary)"
 DIR="$(dirname $0)"
 ncite="$(python $DIR/scholar.py -c 1 -t --phrase "$title" | awk '/Citations [0-9]/ { print $2 }')"
 
+if [ -z $ncite ]; then
+	xmessage -buttons "Ok:0" -default Ok -nearmouse "Paper not found" -timeout 5
+	exit 0
+fi
+
 message="No. citations: $ncite"
 xmessage -buttons "Ok:0,Open in browser:1" -default Ok -nearmouse "$message" -timeout 10
 response=$?
